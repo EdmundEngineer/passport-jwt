b27e7656edc2c9e3b3ee498dc65ebccfd7054b29
Edmund044 <edmundopiyo@gmail.com>
2022-01-26 17:52:40 +0300

    joi validation works

--- controllers/joi_validation.js
+++ controllers/joi_validation.js
@@ -1,47 +1,97 @@
 const Joi = require('joi');
-exports.validateSignUp = (mail, link) => {
-    const schema = Joi.object({
-        username: Joi.string()
-            .alphanum()
-            .min(3)
-            .max(30)
-            .required(),
-    
-        password: Joi.string()
-            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    
-        repeat_password: Joi.ref('password'),
-    
-        access_token: [
-            Joi.string(),
-            Joi.number()
-        ],
-    
-        birth_year: Joi.number()
-            .integer()
-            .min(1900)
-            .max(2013),
-    
+exports.validateSignUp = (mail, password_sent) => {
+    const schema = Joi.object({      
         email: Joi.string()
-            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-    })
-        .with('username', 'birth_year')
-        .xor('password', 'access_token')
-        .with('password', 'repeat_password');
-    
-    
-    schema.validate({ username: 'abc', birth_year: 1994 });
-    // -> { value: { username: 'abc', birth_year: 1994 } }
-    
-    schema.validate({});
-    // -> { value: {}, error: '"username" is required' }
-    
-    // Also -
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+    }).with('email', 'password');
     
     try {
-        const value = await schema.validateAsync({ username: 'abc', birth_year: 1994 });
+        const value =  schema.validateAsync({ email: mail, password: password_sent  });
+        return value;
     }
-    catch (err) { }
+    catch (err) { 
+        return err;
+    }
 }
+exports.validateSignIn = (mail, password_sent) => {
+    const schema = Joi.object({      
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+    }).with('email', 'password');
+    
+    try {
+        const value =  schema.validateAsync({ email: mail, password: password_sent  });
+        return value;
+    }
+    catch (err) { 
+        return err;
+    }
+}
+exports.validateForgot = (mail) => {
+    const schema = Joi.object({      
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
+    });
+    
+    try {
+        const value =  schema.validateAsync({ email: mail });
+        return value;
+    }
+    catch (err) { 
+        return err;
+    }
+}
+exports.validateReset = (password1, password2) => {
+    const schema = Joi.object({      
+        password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+
+        repeat_password: Joi.ref('password')
+    });
+    
+    try {
+        const value =  schema.validateAsync({ password: password1, repeat_password:password2  });
+        return value;
+    }
+    catch (err) { 
+        return err;
+    }
+}
+exports.validateOtp = (otp_sent) => {
+    const schema = Joi.object({  
+          
+        otp:  Joi.string()
+        .length(4)
+        .required()
+    });
+    
+    try {
+        const value =  schema.validateAsync({ otp: otp_sent });
+        return value;
+    }
+    catch (err) { 
+        return err;
+    }
+}
+
+/*exports.validateLogin = (mail, password_sent) => {
+    const schema = Joi.object({      
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    }).with('email', 'password');
+    
+    try {
+        const value =  schema.validateAsync({ email: mail, password: password_sent  });
+        return value;
+    }
+    catch (err) { 
+        return err;
+    }
+}*/
 exports.validateLogin = (mail, link) => {}
-exports.validateLogin = (mail, link) => {
